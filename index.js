4ddbc01cc147bd5217075b2f8c985a67464c63e4
meidanzhu <meidan.zhu@gmail.com>
2021-03-14 17:36:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+ function writeCards(names, event) {
+    let thankYouMessages = []
+    for ( let i = 0; i < names.length; i++) {
+        thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+
+    return thankYouMessages
+ }
+
+ function countDown(number) {
+     while (number > 0 ) {
+         console.log(number)
+         number -= 1
+     }
+     console.log(number)
+ }
